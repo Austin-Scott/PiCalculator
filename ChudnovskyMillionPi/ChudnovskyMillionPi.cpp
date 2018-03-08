0323cb423a5db0cdf82182fd41d9f17dfe8aa8d7
Austin-Scott <12504656+Austin-Scott@users.noreply.github.com>
2018-03-07 22:13:53 -0600

    Optimized code to work with multiple threads

--- ChudnovskyMillionPi/ChudnovskyMillionPi.cpp
+++ ChudnovskyMillionPi/ChudnovskyMillionPi.cpp
@@ -23,6 +23,63 @@ mpz_class pow(mpz_class a, mpz_class b) {
 	return result;
 }
 
+//SNode
+struct SNode {
+	mpq_class S;
+	SNode* next;
+};
+SNode* SHead = nullptr;
+SNode* STail = nullptr;
+mutex StailLock;
+mutex SheadLock;
+atomic<int> SnodeCount = 0;
+void addS(const mpq_class &value) {
+	static SNode* setHead = nullptr;
+	SNode* node = new SNode();
+	node->next = nullptr;
+	node->S = value;
+	StailLock.lock();
+	if (STail == nullptr) {
+		SheadLock.lock();
+		SHead = node;
+		setHead = node;
+		SheadLock.unlock();
+		STail = node;
+	}
+	else {
+		bool isSame = STail == setHead;
+		if (isSame)
+			SheadLock.lock();
+		STail->next = node;
+		STail = node;
+		if (isSame)
+			SheadLock.unlock();
+	}
+	SnodeCount++;
+	StailLock.unlock();
+}
+mpq_class getDeltaS() {
+	SheadLock.lock();
+	while (SHead == nullptr || SHead->next == nullptr) {
+		SheadLock.unlock();
+		this_thread::sleep_for(chrono::milliseconds(10));
+		SheadLock.lock();
+	}
+	mpq_class result = SHead->S;
+	SNode* toDelete = SHead;
+	SHead = SHead->next;
+	delete toDelete;
+	SnodeCount--;
+	if (SHead == nullptr) {
+		StailLock.lock();
+		STail = nullptr;
+		StailLock.unlock();
+	}
+	SheadLock.unlock();
+	return result;
+}
+//SNode
+
 //Prevent multiple threads from writting and reading from mpq_class S at the same time
 mutex sumGuard;
 
@@ -32,28 +89,44 @@ int main() {
 
 	atomic<int> piCount = 0;
 
-	mpz_class K = 6;
-	mpq_class M = 1;
-	mpz_class M1;
-	mpz_class M2;
-	mpz_class L = 13591409;
-	mpz_class X = 1;
 	mpq_class S = 13591409;
 
-	mpz_class k = 1;
-
-	thread t1([&]() {
+	//Compute M values and Partial Sum deltas
+	thread t0([&]() {
+		mpz_class k = 1;
+		mpz_class K = 6;
+		mpz_class L = 13591409;
+		mpz_class M1;
+		mpz_class M2;
+		mpq_class M=1;
 		while (piCount < 1000000) {
+			while (!(SnodeCount < 10)) this_thread::sleep_for(chrono::milliseconds(10));
 			M1 = pow(K, (mpz_class)3);
 			M2 = pow(k, (mpz_class)3);
-			M = (mpq_class)(M1 - (K << 4))*M / M2;
+			M=(mpq_class)(M1 - (K << 4))*(M / M2);
+
 			L += 545140134;
-			X *= -262537412640768000;
+			addS(M*L);
+
 			K += 12;
 			k++;
+		}
+	});
+
+	//Compute Sum values from M values and partial Sum deltas
+	thread t1([&]() {
+		mpz_class k = 1;
+		mpz_class X = 1;
+		while (piCount < 1000000) {
+
+			X *= -262537412640768000;
+			
+			k++;
+
+			mpq_class sumDelta = getDeltaS() / X;
 
 			sumGuard.lock();
-			S += (M*L) / X;
+			S += sumDelta;
 			sumGuard.unlock();
 		}
 	});
@@ -70,9 +143,10 @@ int main() {
 
 		piCount = piPrintCount(pi, piCount);
 
-		//this_thread::sleep_for(chrono::milliseconds(10));
 
 	}
+	t0.join();
+	t1.join();
 
 	cout << endl << "One million digits of pi have been calculated! Happy pi day! :)" << endl;
 	system("pause");
