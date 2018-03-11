bc5dd25f00a7f4fde12aee82ecf7e47c2b15429e
Austin-Scott <12504656+Austin-Scott@users.noreply.github.com>
2018-03-10 22:04:47 -0600

    High performance BSA Chudnovsky is now working

--- BinarySplitChudnovskyPi/BinarySplitChudnovskyPi.cpp
+++ BinarySplitChudnovskyPi/BinarySplitChudnovskyPi.cpp
@@ -7,12 +7,12 @@
 
 using namespace std;
 
-mpz_class C3_24 = mpz_class("10939058860032000");
 mpz_class A = 13591409;
 mpz_class B = 545140134;
 mpz_class C = 640320;
 mpz_class D = 426880;
 mpz_class E = 10005;
+mpz_class C3_24 = C * C * C / 24;
 
 const double DIGITS_PER_ITERATION = 14.1816474627254776555;
 int targetDigits = 1000000;
@@ -21,31 +21,29 @@ struct PQT {
 	mpz_class P, Q, T;
 };
 
+PQT operator+(const PQT &a, const PQT &b) {
+	PQT result;
+	result.P = a.P * b.P;
+	result.Q = a.Q * b.Q;
+	result.T = b.Q * a.T + a.P * b.T;
+	return result;
+}
+
 PQT binaryChudnovsky(int a, int b) {
 	PQT result;
 	if (b == a + 1) {
-		if (a == 0) {
-			result.P = 1;
-			result.Q = 1;
-		}
-		else {
-			result.P = (6 * a - 5);
-			result.P *= (2 * a - 1);
-			result.P *= (6 * a - 1);
-			result.Q = C3_24*b*b*b;
-		}
+		result.P = (6 * b - 5);
+		result.P *= (2 * b - 1);
+		result.P *= (6 * b - 1);
+		result.Q = C3_24*b*b*b;
 		result.T = result.P * (A + B * b);
-		if ((b & 1)==1) {
+		if (b & 1) {
 			result.T = -result.T;
 		}
 	}
 	else {
 		int m = (a + b) / 2;
-		PQT am = binaryChudnovsky(a, m);
-		PQT mb = binaryChudnovsky(m, b);
-		result.P = am.P * mb.P;
-		result.Q = am.Q * mb.Q;
-		result.T = mb.Q * am.T + am.P * mb.T;
+		result = binaryChudnovsky(a, m) + binaryChudnovsky(m, b);
 	}
 	return result;
 }
